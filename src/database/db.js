fdb2f6cd7378995a4b9d5870de7158f136903dd6
JuampaVLB <contact.juampavlb@gmail.com>
2023-09-29 11:25:02 -0600

    added search on stock

--- src/database/db.js
+++ src/database/db.js
@@ -4,7 +4,7 @@ const user = 'JuampaVLB';
 
 const password = 'TlThRUmQWNVA785n';
 
-const URI = `mongodb+srv://${user}:${password}@cluster0.ktoxf8e.mongodb.net/escuela`;
+const URI = `mongodb://0.0.0.0:27017/escuela`;
 
 // Local
 // mongodb://0.0.0.0:27017/escuela
